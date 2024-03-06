52b4c7c6fc3c55b4b5bac7f968a810cb17c964f4
Tamires Britos <84687414+tahbritos@users.noreply.github.com>
2024-03-05 21:45:57 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+//let titulo = document.querySelector('h1');
+//titulo.innerHTML = 'Jogo do número secreto';
+
+//let paragrafo = document.querySelector('p');
+//paragrafo.innerHTML = 'Escolha um número entre 1 a 10';
+
+let numeroSecreto = 6;
+
+function exibirTextoNaTela (tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 a 10');
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+
+    if (chute == numeroSecreto){
+        exibirTextoNaTela('h1', 'Acertou!');
+        exibirTextoNaTela('p', 'Você descobriu o número secreto!');
+    } else {
+        if (chute > numeroSecreto){
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+    }
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 100 + 1);
+}
